95c539fc1d01e5e63ee16d691e0199493422fe4e
JiriKrizek <jkrizek@gmail.com>
2013-02-12 21:53:10 +0100

    Added pathogen and command-T install to install.sh

--- install.sh
+++ install.sh
@@ -29,4 +29,23 @@ else
   echo "...Error: ~/.vimrc already exists"
 fi
 
+echo "Installing Pathogen"
+if [[ ! -f ~/.vim/autoload/pathogen.vim ]]; then
+  mkdir -p ~/.vim/autoload ~/.vim/bundle; \
+  curl -Sso ~/.vim/autoload/pathogen.vim \
+        https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+  echo "...Done"
+else
+  echo "...Pathogen already exists"
+fi
+
+echo "Installing command-T"
+if [[ ! -d ~/.vim/command-t ]]; then
+  cd ~/.vim
+  git clone git://git.wincent.com/command-t.git bundle/command-t
+  echo "...Done"
+else
+  echo "...Command-T already exists"
+fi
+
 
